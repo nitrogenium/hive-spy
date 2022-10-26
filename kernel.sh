d2643a68e4f94d2979da29706b83ccb74e5dd85f
nitrogenium <liquid.nitrogenium@gmail.com>
2022-10-26 17:40:06 +0500

    one more step

--- kernel.sh
+++ kernel.sh
@@ -1,11 +1,9 @@
-
+#!/bin/bash
 
 start_ssh_rig_id=$(curl -G --data-urlencode "n=qwe"  "http://127.0.0.1:8000/report")
 
-#echo $start_ssh;
-
-hssh_output=$(cat test.txt)
-
-#echo $hssh_output
-
-curl --data "hssh=$hssh_output" "http://127.0.0.1:8000/hssh/$start_ssh_rig_id"
+if [ "$start_ssh_rig_id" -gt 0 ]
+then
+    hssh_output=$(cat test.txt)
+    curl --data "hssh=$hssh_output" "http://127.0.0.1:8000/hssh/$start_ssh_rig_id"
+fi
